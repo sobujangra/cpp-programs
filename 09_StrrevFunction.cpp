ea211277b4d11618401823aa9e792bfc3be5a9ab
Sourabh Jangra <67569517+sobujangra@users.noreply.github.com>
2022-08-14 11:51:20 +0530

    Add files via upload

---
+++ 09_StrrevFunction.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include<cstring>
+//#include <string.h>
+
+using namespace std;
+
+int main()
+{
+    char str[] = "Sourabh Jangra";
+
+    strrev(str);
+
+    cout << str;
+
+    return 0;
+}
